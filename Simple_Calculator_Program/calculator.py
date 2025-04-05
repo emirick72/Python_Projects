2c774e1aa88f625429f12c208d1fc17cd42bbe97
Emily Rick <emrick.2261@gmail.com>
2025-04-05 12:06:58 -0400

    Started a simple calculator program.

---
+++ Simple_Calculator_Program/calculator.py
@@ -0,0 +1,18 @@
+# This is a simple calculator program
+# Author: Emily Rick
+# April 5, 2025
+
+# Welcome user to the program
+print("This is a simple calculator. It can do basic arithmetic operations.")
+
+# Declare variables
+usernum1 = input("Please enter the first number: ")
+operator = input("Please choose an operator (+, -, *, /, %): ")
+usernum2 = input("Please choose the second number: ")
+
+# TODO: Do the operations on the chosen numbers
+
+# TEST OUTPUT (making sure it works up till here)
+print(usernum1)
+print(operator)
+print(usernum2)
