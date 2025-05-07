45e9a06d14ba9a8124baee6eb47a29f6ee028a89
Emily Rick <emrick.2261@gmail.com>
2025-05-07 00:46:19 -0400

    Started working on how the "choose your character" logic would work.

--- Pygame Stuff/FNAF_Visual_Novel_Project_TEST/test.py
+++ Pygame Stuff/FNAF_Visual_Novel_Project_TEST/test.py
@@ -1,1 +1,39 @@
-# THIS IS A TEST FILE FOR MY FNAF VISUAL NOVEL PROJECT
+'''
+THIS IS A TEST FILE FOR MY FNAF VISUAL NOVEL PROJECT
+Ideally, I want the final project to be made using Pygame,
+but for now I'm using the console to see what the output would look like.
+'''
+
+# Welcome the player
+print("Welcome to (Untitled FNAF Fangame)!")
+
+# character variables
+Mike = "Mike Schmidt"
+Jeremy = "Jeremy Fitzgerald"
+Vanessa = "Vanessa Shelly"
+
+# Choose your character
+while True:
+
+    try:
+        #player_character = input("Choose a character to play as:")
+        print("Choose a character to play as:")
+        print("   1. " + Mike)
+        print("   2. " + Jeremy)
+        print("   3. " + Vanessa)
+
+        player_character = input()
+        player_choice = int(player_character) # convert input (string) to int
+        if player_choice == 1:
+            print("You chose Mike Schmidt")
+            break
+        elif player_choice == 2:
+            print("You chose Jeremy Fitzgerald")
+            break
+        elif player_choice == 3:
+            print("You chose Vanessa Shelly")
+            break
+        else:
+            print("That input is not valid. PLease enter a valid number (1, 2, or 3).")
+    except ValueError:
+        print("Please enter a number:")
